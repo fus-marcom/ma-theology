f5350e69804c079d2a1375f43ee72f7c1d448a1f
Jesse R Weigel <jesse@jesseweigel.com>
2016-07-01 16:47:44 -0400

    Optimize load times

--- app/js/app.js
+++ app/js/app.js
@@ -94,6 +94,10 @@ $(window).scroll(function(){
       $replay = $('#replay'),
       $pause = $('#pause');
 
+  if ($(window).width() > 800) {
+    $vid[0].play();
+  }
+
   $pause.hide();
 
   $vid.on('playing',function(){
