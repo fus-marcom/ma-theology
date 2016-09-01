c207874bc04cbb38a7dc3adfb9111a1f94fb8efb
Jesse R Weigel <jesse@jesseweigel.com>
2016-09-01 14:42:26 -0400

    Update Facebook tracking pixel

--- app/js/analytics.js
+++ app/js/analytics.js
@@ -1,19 +1,19 @@
-<!-- Facebook Conversion Code for main site conversion tracking pixel -->
+<!-- Facebook Conversion Code for MA Theology Microsite -->
 <script>(function() {
-var _fbq = window._fbq || (window._fbq = []);
-if (!_fbq.loaded) {
-var fbds = document.createElement('script');
-fbds.async = true;
-fbds.src = 'https://connect.facebook.net/en_US/fbds.js';
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(fbds, s);
-_fbq.loaded = true;
-}
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = 'https://connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
 })();
 window._fbq = window._fbq || [];
-window._fbq.push(['track', '6010544466243', {'value':'0.00','currency':'USD'}]);
+window._fbq.push(['track', '6053812137643', {'value':'0.01','currency':'USD'}]);
 </script>
-<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6010544466243&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6053812137643&amp;cd[value]=0.01&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
   <script>
